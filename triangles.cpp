7d48ec3e89f8e011107bf9765757ea42e0811b5a
Maria <st089778@student.spbu.ru>
2022-07-05 19:29:28 +0300

    final

--- triangles.cpp
+++ triangles.cpp
@@ -47,19 +47,33 @@ void two(){
     }
 }
 
-void snakeArray(int** A, int n, int dir){
-    int k=1;
-    for(int i=0; i<n; ++i)
-        if (!(i%2))
-            for(int j=0;j<n;++j) {
-                A[i][j] = k;
-                ++k;
-            }
-        else
-            for(int j=n-1;j>=0;--j) {
-                A[i][j]=k;
-                ++k;
-            }
+void snakeArray(int** A, int n, int right) { //или вправо или вниз
+    int k = 1;
+    if (right){
+        for (int i = 0; i < n; ++i)
+            if (!(i % 2))
+                for (int j = 0; j < n; ++j) {
+                    A[i][j] = k;
+                    ++k;
+                }
+            else
+                for (int j = n - 1; j >= 0; --j) {
+                    A[i][j] = k;
+                    ++k;
+                }
+}
+    else
+        for(int j=0; j<n; ++j)
+            if (!(j%2))
+                for(int i=0;i<n;++i) {
+                    A[i][j] = k;
+                    ++k;
+                }
+            else
+                for(int i=n-1;i>=0;--i) {
+                    A[i][j]=k;
+                    ++k;
+                }
 }
 
 double mysqrt(double x, double eps=1e-6){
@@ -72,10 +86,11 @@ double mysqrt(double x, double eps=1e-6){
     return z_2;
 }
 
-void snail(int **A, int n, int m){
+void snail(int **A, int n, int m, int dir){ // 1 - down 0 - right
     int counter=0;
     int k=1;
-    while(k<n*m) { //придется отдельно писать одномерный случай
+    if (dir)
+    while(k<=n*m) {
         //сверху слева направо
         for (int i = counter; i < n - counter; ++i) {
             A[i][counter] = k;
@@ -102,6 +117,34 @@ void snail(int **A, int n, int m){
     }
         ++counter;
     }
+    else
+        while(k<=n*m) {
+            //сверху слева направо
+            for (int j = counter; j < m - counter; ++j) {
+                A[counter][j] = k;
+                ++k;
+            }
+            //справа сверху вниз
+            for (int i = counter + 1; i < n - counter - 1; ++i) {
+                if (k<=n*m){
+                    A[i][m - counter - 1] = k;
+                    ++k;}
+            }
+            //снизу справа налево
+            for (int j = m - counter - 1; j > counter; --j) {
+                if (k<=n*m){
+                    A[n - counter - 1][j] = k;
+                    ++k;}
+            }
+            //слева снизу вверх
+            for (int i = n - counter - 1; i > counter; --i){
+                if (k<=n*m){
+                    A[i][counter] = k;
+                    ++k;
+                }
+            }
+            ++counter;
+        }
 }
 
 void six(){
@@ -140,58 +183,64 @@ void shiftArray(int A[], int n, int k)
         cout << A[i] << " ";
 }
 
-void eight(int **A, int n, int m){
+void eight(int **A, int n, int m, int dir){ //1 - down -1 - right
     int k=1;
-
+    int c=dir;
     int i=0;int j=0;
     A[i][j]=k;++k;
     A[n-1][m-1]=n*m; //маленький костыль чтобы не писать большой
+        while (k < n * m) {
+            if(dir==1){
+            if ((i + 1 < n && j - 1 < 0) || (i + 1 < n && j + 1 >= m)) {
+                ++i;
+                A[i][j] = k;
+                ++k;
+            } else if ((j + 1 < m && i - 1 < 0) || (j + 1 < m && i + 1 >= n)) {
+                ++j;
+                A[i][j] = k;
+                ++k;
+            }}
+            else {
+                if ((j + 1 < m && i - 1 < 0) || (j + 1 < m && i + 1 >= n)) {
+                    ++j;
+                    A[i][j] = k;
+                    ++k;
+                }
+                else if ((i + 1 < n && j - 1 < 0) || (i + 1 < n && j + 1 >= m)) {
+                    ++i;
+                    A[i][j] = k;
+                    ++k;
+                }
+            }
+            if (c==1){c=c*(-1);
+            while (0 <= i - 1 && j < m - 1) {
+                --i;
+                ++j;
+                A[i][j] = k;
+                ++k;
+            }
+                }
+            else if (c==-1){  c=c*(-1);
+                while (i + 1 < n && j > 0) {
+                    ++i;
+                    --j;
+                    A[i][j] = k;
+                    ++k;
 
-    while(k<n*m) {
+                }}
 
-        if ((i + 1 < n && j - 1 < 0)|| (i + 1 < n && j+1 >= m)) {
-            ++i;
-            A[i][j] = k;
-            ++k;
         }
-
-        else if ( (j + 1 < m && i - 1 < 0) || (j + 1 < m && i+1 >= n ) ) {
-            ++j;
-            A[i][j] = k;
-            ++k;
-        }
-
-        while (0 <= i-1 && j < m-1)
-        {--i;++j; A[i][j]=k;++k;}
-
-        if ((i + 1 < n && j-1 < 0) || (i + 1 < n && j+1 >= m)) {
-            ++i;
-            A[i][j] = k;
-            ++k;
-        }
-
-        else if ((j + 1 < m && i + 1 >= n) || (j + 1 < m && i-1 < 0 ) ) {
-            ++j;
-            A[i][j] = k;
-            ++k;
-        }
-
-        while (i+1 < n && j > 0)
-        {++i;--j; A[i][j]=k;++k;}
-
-    }
-
 }
 
 
 int main(){
-    int n=15; int m=10;
+    int n=5; int m=5;
     int** A=new int*[n];
     for(int i = 0; i < n; i++){
         A[i] = new int[m];
     }
 
-    eight(A,n,m);
+    eight(A,n,m,-1);
 
     for(int i=0;i<n;++i) {
         for (int j = 0; j < m; ++j)
