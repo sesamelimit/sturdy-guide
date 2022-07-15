6ba916f1864339873b4ff5b2f01a77700a652826
Maria Guseva <75927476+oreshkichikaladnie@users.noreply.github.com>
2022-07-15 11:32:41 +0300

    Update sailstar.cpp

--- sailstar.cpp
+++ sailstar.cpp
@@ -1,6 +1,7 @@
 #import <math.h>
 #import <iostream>
 using namespace std;
+#define _USE_MATH_DEFINES
 
 bool sail(double x, double y){
     if (x*x + y*y <=4 && y>=-1){
@@ -21,11 +22,12 @@ bool sail(double x, double y){
 
 }
 
-
-
-int main(){
-    double x=10;
-    double y=10;
-    cout << sail(x,y);
-    return 0;
+bool star(double x, double y){
+    if (((int)(tan(72*M_PI/180)*abs(y) <= 5-x) +
+    (int)(tan(72*M_PI/180)*abs(-sin(144*M_PI/180)*x+cos(144*M_PI/180)*y)<=5-cos(144*M_PI/180)*x-sin(144*M_PI/180)*y)+
+    + (int)(tan(72*M_PI/180)*abs(-sin(72*M_PI/180)*x+cos(72*M_PI/180)*y)<=5-cos(72*M_PI/180)*x-sin(72*M_PI/180)*y)
+    +(int)(tan(72*M_PI/180)*abs(sin(72*M_PI/180)*x+cos(72*M_PI/180)*y)<=5-cos(72*M_PI/180)*x+sin(72*M_PI/180)*y))>1)
+        return true;
+    else
+        return false;
 }
