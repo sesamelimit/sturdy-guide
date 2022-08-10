717f6881f657e76c1c06ff00e03fbf4c09c32a0e
Мария <@DESKTOP-1I0M1JU>
2022-08-10 14:50:37 +0300

    dyn arrays

--- dyn_arrays.cpp
+++ dyn_arrays.cpp
@@ -1,93 +1,84 @@
 #include <iostream>
-using namespace std;
 #include <random>
+#define rows 5
+#define columns 6
 
-void one() {
-    int n; //высота столбца
-    int m; //длина строки
+unsigned int** one() {
+    size_t n=rows; //высота столбца
+    size_t m=columns; //длина строки
     int direction;
-    cout << "input n and m" << endl;
-    cin >> n;
-    cin >> m;
-    cout << "0 for right ; 1 for left" << endl;
-    cin >> direction;
-    int A[n][m];
+   /* std::cout << "input n" << std::endl;
+    std::cin >> n;
+    std::cout << "input m" << std::endl;
+    std::cin >> m;
+    std::cout << "0 for right ; 1 for left" << std::endl;
+    */std::cin >> direction;
+
+    unsigned int** A = new unsigned int* [n];
+    for (size_t i = 0; i < n; i++) {
+        A[i] = new unsigned int[m];
+    }
+
     int k=1;
-    for(int i = 0; i < n; ++i)
+    for(size_t i = 0; i < n; ++i)
         if (!((i + direction) % 2))
-            for(int j = 0;j < m;++j)
+            for(size_t j = 0;j < m;++j)
             {
                 A[i][j]=k;
                 ++k;
             }
         else
-            for(int j = m - 1;j >= 0;--j)
+            for(size_t j = m - 1;j != -1;--j)
             {
                 A[i][j]=k;
                 ++k;
             }
+    return A;
 
 }
 
-void two(){
-    random_device dev;
-    mt19937 gen(dev());
-    uniform_int_distribution<> distr(0, 100000);
+unsigned int** two(){
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_int_distribution<> distr(0, 100000);
 
-    int n;
-    int m;
-    cout << "input n and m" << endl;
-    cin >> n;
-    cin >> m;
-    unsigned int A[n][m];
-    for(int i = 0; i < n;++i)
-        for(int j = 0;j < m;++j)
+    size_t n=rows;
+    size_t m=columns;
+    /*std::cout << "input n" << std::endl;
+    std::cin >> n;
+    std::cout << "input m" << std::endl;
+    std::cin >> m;*/
+
+    unsigned int** A = new unsigned int* [n];
+    for (size_t i = 0; i < n; i++) {
+        A[i] = new unsigned int[m];
+    }
+
+    for(size_t i = 0; i < n;++i)
+        for(size_t j = 0;j < m;++j)
             A[i][j]=distr(gen);
-    for(int i = 0; i < n;++i)
-    {
-        for (int j = 0; j < m; ++j)
-            cout << "\t" << A[i][j];
-        cout << endl;
-    }
+    return A;
 }
 
-void snakeArray(int** A, int n, int right) { //или вправо или вниз
+void snakeArray(int** A, size_t n, size_t right) { //или вправо или вниз
     int k = 1;
-    if (right){
-        for (int i = 0; i < n; ++i)
-            if (!(i % 2))
-                for (int j = 0; j < n; ++j)
+        for (size_t i = 0; i < n; ++i)
+                for (size_t j = (n-1)*(i%2); 0 <=j && j < n; j += (-1)*(i%2) + (i+1)%2 )
                 {
-                    A[i][j] = k;
+                    if (right) 
+                        A[i][j] = k;
+                    else
+                        A[j][i] = k;
                     ++k;
                 }
-            else
-                for (int j = n - 1; j >= 0; --j)
-                {
-                    A[i][j] = k;
-                    ++k;
-                }
-    }
-    else
-        for(int j = 0; j < n; ++j)
-            if (!(j % 2))
-                for(int i = 0;i < n;++i)
-                {
-                    A[i][j] = k;
-                    ++k;
-                }
-            else
-                for(int i = n - 1;i >= 0;--i)
-                {
-                    A[i][j]=k;
-                    ++k;
-                }
+                
+
 }
 
 double mysqrt(double x, double eps=1e-6){
     double z_1 = 1;
     double z_2 = z_1 - (z_1 * z_1 - x) / (2 * z_1);
-    while( abs(z_2 - z_1) > eps)
+    while( std::abs(z_2 - z_1) > eps)
     {
         z_1 = z_2;
         z_2 = z_1 - (z_1 * z_1 - x) / (2 * z_1);
@@ -95,114 +86,91 @@ double mysqrt(double x, double eps=1e-6){
     return z_2;
 }
 
-void snail(int **A, int n, int m, int dir){ // 1 - down 0 - right
+void snail(int **A, size_t n, size_t m, int dir){ // 1 - down 0 - right
     int counter = 0;
     int k = 1;
-    if (dir)
         while(k <= n*m) {
             //сверху слева направо
-            for (int i = counter; i < n - counter; ++i)
+            for (size_t i = counter; i < dir*n + ((dir+1)%2)*m - counter; ++i)
             {
-                A[i][counter] = k;
+                if (dir) A[i][counter] = k;
+                else A[counter][i] = k;
                 ++k;
             }
             //справа сверху вниз
-            for (int j = counter + 1; j < m - counter - 1; ++j) {
+            for (size_t j = counter + 1; j < dir*m + ((dir + 1) % 2) *n - counter - 1; ++j) {
                 if (k <= n * m){
-                    A[n - counter - 1][j] = k;
+                    if (dir) A[n - counter - 1][j] = k;
+                    else A[j][m - counter - 1] = k;
                     ++k;
                 }
             }
             //снизу справа налево
-            for (int i = n - counter - 1; i > counter; --i) {
+            for (size_t i = dir * n + ((dir + 1) % 2) * m - counter - 1; i > counter; --i) {
                 if (k <= n * m){
-                    A[i][m - counter - 1] = k;
+                    if(dir) A[i][m - counter - 1] = k;
+                    else A[n - counter - 1][i] = k;
                     ++k;
                 }
             }
             //слева снизу вверх
-            for (int j = m - counter - 1; j > counter; --j){
+            for (size_t j = dir * m + ((dir + 1) % 2) * n - counter - 1; j > counter; --j){
                 if (k <= n*m){
-                    A[counter][j] = k;
+                    if(dir) A[counter][j] = k;
+                    else A[j][counter] = k;
                     ++k;
                 }
             }
 
             ++counter;
         }
-    else
-        while(k <= n * m) {
-            //сверху слева направо
-            for (int j = counter; j < m - counter; ++j) {
-                A[counter][j] = k;
-                ++k;
-            }
-            //справа сверху вниз
-            for (int i = counter + 1; i < n - counter - 1; ++i) {
-                if (k <= n * m){
-                    A[i][m - counter - 1] = k;
-                    ++k;
-                }
-            }
-            //снизу справа налево
-            for (int j = m - counter - 1; j > counter; --j) {
-                if (k <= n * m){
-                    A[n - counter - 1][j] = k;
-                    ++k;
-                }
-            }
-            //слева снизу вверх
-            for (int i = n - counter - 1; i > counter; --i){
-                if (k <= n * m){
-                    A[i][counter] = k;
-                    ++k;
-                }
-            }
-            ++counter;
-        }
+    
 }
 
-void six(){
-    random_device dev;
-    mt19937 gen(dev());
-    uniform_int_distribution<> distr(0, 100000);
-    int n;
-    int m;
-    cout << "input n and m" << endl;
-    cin >> n;
-    cin >> m;
-    unsigned int A[n][m];
-    for(int i = 0; i < n; ++i)
-        for(int j = 0; j < m; ++j)
+unsigned int** six(){
+    std::random_device dev;
+    std::mt19937 gen(dev());
+    std::uniform_int_distribution<> distr(0, 100000);
+    size_t n=rows;
+    size_t m=columns;
+   /* std::cout << "input n" << std::endl;
+    std::cin >> n;
+    std::cout << "input m" << std::endl;
+    std::cin >> m;*/
+
+    unsigned int** A = new unsigned int* [n];
+    for (size_t i = 0; i < n; i++) {
+        A[i] = new unsigned int[m];
+    }
+    for(size_t i = 0; i < n; ++i)
+        for(size_t j = 0; j < m; ++j)
             A[i][j]=distr(gen);
-    for(int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
+    for(size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < m; ++j) {
             std::cout.width(6);
             std::cout << std::right << A[i][j];
         }
-        cout << endl;
+        std::cout << std::endl;
     }
+    return A;
 }
 
 void shiftArray(int A[], int n, int k)
 {
-    int temp[k];
-    for(int i = 0; i < k; ++i)
-        temp[i]=A[i];
-
-    for(int i = 0; i < n - k; ++i)
-        A[i]=A[i+k];
-    for(int i = 0; i < k; ++i)
-        A[n - k + i]=temp[i];
-    for(int i = 0; i < n; ++i)
-        cout << A[i] << " ";
+    for (size_t t = 1; t <= k; ++t)
+    {
+        int temp = A[0];
+        for (size_t i = 0; i < n-1; ++i)
+            A[i] = A[i+1];
+        A[n - 1] = temp;
+    }
 }
 
-void eight(int **A, int n, int m, int dir){ //1 - down -1 - right
+void eight(int **A, size_t n, size_t m, int dir){ //1 - down -1 - right
     int k=1;
     int c=dir;
-    int i=0;
-    int j=0;
+    size_t i=0;
+    size_t j=0;
     A[i][j]=k;
     ++k;
     A[n-1][m-1]=n*m; //маленький костыль чтобы не писать большой
@@ -235,7 +203,7 @@ void eight(int **A, int n, int m, int dir){ //1 - down -1 - right
         }
 
         if (c == 1){
-            c = c * (-1);
+            c *= -1;
             while (0 <= i - 1 && j < m - 1) {
                 --i;
                 ++j;
@@ -245,7 +213,7 @@ void eight(int **A, int n, int m, int dir){ //1 - down -1 - right
         }
 
         else if (c == -1){
-            c = c * (-1);
+            c *= -1;
             while (i + 1 < n && j > 0) {
                 ++i;
                 --j;
@@ -254,4 +222,19 @@ void eight(int **A, int n, int m, int dir){ //1 - down -1 - right
             }
         }
     }
+}
+
+int main() {
+    size_t n = rows;
+    size_t m = columns;
+    int C[rows];
+    for (size_t i = 0; i < rows; ++i)
+        C[i] = i;
+    for (size_t i = 0; i < rows; ++i)
+        std::cout << C[i]<<" ";
+    std::cout << std::endl;
+    shiftArray(C, n, 3);
+    for (size_t i = 0; i < rows; ++i)
+        std::cout << C[i];
+    return 0;
 }
